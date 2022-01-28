cc203fb9d1c5f929d169015c0b14933b4b9cf0f6
ehiz briel <ehisucre@gmail.com>
2022-01-28 17:27:48 +0000

    changes made to webpack config, html and base index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-
-    render(){
-        return (
-            <div>
-                <h1 className="text-3xl font-bold underline">testing</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">testing</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
