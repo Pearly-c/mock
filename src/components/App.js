2ecbf13ab5969c678381715bfa15dd6c5f463273
pearly-c <pearlchristian6@gmail.com>
2022-01-28 16:47:11 +0100

    react setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1 className="text-3xl font-bold underline">testing</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
